8161dd971877cf8cf7a8c1beda4574ae839b41d1
huikal <127702698+huikal@users.noreply.github.com>
2024-08-08 16:55:12 +0900

    Delete Firebase directory

--- Firebase/app/src/main/java/com/example/firebase/model/ItemData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.firebase.model
-
-class ItemData {
-    var docId: String? = null
-    var email: String? = null
-    var content: String? = null
-    var date: String? = null
-}
